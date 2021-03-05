be2618ff7d4e5ce4aa0099605924ac1dedeba771
Sergey <me@sergeyg.com>
2021-03-05 14:00:56 -0500

    Map to tokens

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
 import { DelegatingReader } from "./read/readers/DelegatingReader";
 import { Read } from "./read/Read";
+import { Reader } from "./read/Reader";
+
+interface IToken<Type extends string> {
+  type: Type;
+}
+interface WhitespaceToken extends IToken<'whitespace'> {
+  content: string;
+}
+interface IdentifierToken extends IToken<'identifier'> {
+  identifier: string;
+}
+interface NumberLiteral extends IToken<'number'> {
+  value: string;
+}
+type Operator = '=' | '<=' | '>=' | '<' | '>';
+type Literal = NumberLiteral | IdentifierToken;
+interface LiteralList extends IToken<'literal-list'> {
+  literals: Literal[];
+}
+interface EqualityCondition extends IToken<'eq-condition'> {
+  operator: Operator;
+  lhs: IdentifierToken;
+  rhs: Literal;
+}
+interface InCondition extends IToken<'in-condition'> {
+  lhs: IdentifierToken;
+  rhs: LiteralList;
+}
+type Condition = EqualityCondition | InCondition;
+interface OrTokens extends IToken<'ors'> {
+  tokens: (AndTokens | OrTokens)[];
+}
+interface AndTokens extends IToken<'ands'> {
+  tokens: (Condition | OrTokens)[];
+}
+
+// type Token = WhitespaceToken | Literal | LiteralList | EqualityCondition | ;
 
 const openParen = Read.char('(').labeled('open-paren');
 const closeParen = Read.char(')').labeled('close-paren');
@@ -11,28 +48,31 @@ const whitespaceChar =
     .or(Read.char('\t'))
     .or(Read.char('\n'))
     .labeled('whitespace-char');
-const whitespace = whitespaceChar.repeated().labeled('whitespace');
+const whitespace: Reader<WhitespaceToken> = whitespaceChar.repeated().labeled('whitespace')
+  .map(chars => ({type: 'whitespace', content: chars.map(c => c.value).join('')}));
 // The first character of an identifier must be a letter or underscore
 const identifierFirstChar = Read.regexChar(/[a-zA-Z_]/);
 const identifierSubsequentChar = Read.regexChar(/[a-zA-Z0-9_]/);
-const identifier = identifierFirstChar.then(identifierSubsequentChar.repeated())
+const identifier: Reader<IdentifierToken> =
+  identifierFirstChar.then(identifierSubsequentChar.repeated())
   .map((tokens) => tokens[0].value + tokens[1].value.map(t => t.value).join(''))
-  .labeled('identifier');
-const number =
+  .labeled('identifier')
+  .map(id => ({type: 'identifier', identifier: id}));
+const number: Reader<NumberLiteral> =
   Read.regexChar(/\d/).repeated()
   .map((tokens) => tokens.map((token) => token.value).join(''))
-  .labeled('number');
+  .labeled('number')
+  .map(value => ({type: 'number', value: value}));
 
-const literal = identifier.or(number).labeled('literal');
-const literalList = literal
+const literal: Reader<Literal> = identifier.or(number).labeled('literal');
+const literalList: Reader<LiteralList> = literal
   .separatedBy(comma.wrappedBy(whitespace))
   .between(openParen, closeParen)
-  .labeled('literal-list');
+  .labeled('literal-list')
+  // TODO: Preserve position information of the tokens
+  .map(tokens => ({type: 'literal-list', literals: tokens.map(t => t.value)}));
 
-const value = literal.or(identifier)
-  .labeled('value');
-
-const equalityOperator =
+const equalityOperator: Reader<Operator> =
   Read.literal('=')
   .or(Read.literal('<='))
   .or(Read.literal('>='))
@@ -40,19 +80,46 @@ const equalityOperator =
   .or(Read.literal('>'))
   .labeled('equality-op');
 
-const condition = identifier.then(equalityOperator.wrappedBy(whitespace).then(value))
+// TODO: Support literal/literal conditions and complex expressions
+const condition: Reader<EqualityCondition | InCondition> = identifier.then(equalityOperator.wrappedBy(whitespace).then(literal))
   .or(identifier.then(Read.literal('in').wrappedBy(whitespace).then(literalList)))
-  .labeled('condition');
+  .labeled('condition')
+  .map(tokens => {
+    const lhs = tokens[0].value;
+    const rhs = tokens[1].value[1].value;
+    if (tokens[1].value[0].value === 'in') {
+      return {
+        type: 'in-condition',
+        lhs: lhs,
+        rhs: rhs
+      } as InCondition;
+    } else {
+      return {
+        type: 'eq-condition',
+        lhs: lhs,
+        rhs: rhs
+      } as EqualityCondition;
+    }
+  });
 
-const exprDel = new DelegatingReader<any>()
+const exprDel = new DelegatingReader<OrTokens>()
   .labeled('expression');
-const ands = condition
+const ands: Reader<AndTokens> = condition
   .or(exprDel.between(openParen, closeParen))
   .separatedBy(and)
-  .labeled('ands');
-const ors = ands.separatedBy(or)
+  .labeled('ands')
+  .map(tokens => ({
+    type: 'ands',
+    tokens: tokens.map(t => t.value)
+  }));
+const ors: Reader<OrTokens> = ands
   .or(exprDel.between(openParen, closeParen))
-  .labeled('ors');
+  .separatedBy(or)
+  .labeled('ors')
+  .map(tokens => ({
+    type: 'ors',
+    tokens: tokens.map(t => t.value)
+  }));
 
 exprDel.delegate = ors;
 const expr = exprDel.then(Read.eof())
