d30c7b74477f2873dcdd08ef241b3ae939b77c5d
Sergey <me@sergeyg.com>
2021-03-20 17:58:32 -0400

    Add .labeled(...) default options test

--- tests/readers/LabeledReader.test.ts
+++ tests/readers/LabeledReader.test.ts
@@ -7,6 +7,17 @@ describe('LabeledReader', function() {
   const reader = base.labeled('token x', {relabel: false, context: false});
   const relabel = base.labeled('token x', {relabel: true});
   const recontext = base.labeled('token x', {context: true});
+  it('default behavior', function() {
+    expect(base.labeled('token x').read('z', 0))
+      .to.deep.equal({
+        type: 'failure',
+        errors: [{
+          expected: "'x'",
+          position: 0,
+          context: [{label: 'token x', position: 0}]
+        }]
+      })
+  });
   it('relabels failure', function() {
     expect(relabel.read('z', 0))
       .to.deep.equal({
